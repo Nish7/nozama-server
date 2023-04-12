89cc2e9bd6e0b8c482d57272375164cbcb61d38b
Nishil <nishilkapadia17@gmail.com>
2023-04-11 21:43:19 -0400

    changed env

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -8,7 +8,7 @@ module.exports = function (req, res, next) {
 	if (!token) return next(new errResponse('Authorisation Denied', 401));
 
 	try {
-		const decoded = jwt.verify(token, config.get('jwtSecret'));
+		const decoded = jwt.verify(token, process.env.jwtSecret);
 		req.user = decoded.user;
 		next();
 	} catch (err) {
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -52,7 +52,7 @@ router.post(
 
 			jwt.sign(
 				payload,
-				config.get('jwtSecret'),
+				process.env.jwtSecret,
 				{ expiresIn: 3600 },
 				(err, token) => {
 					if (err) {
